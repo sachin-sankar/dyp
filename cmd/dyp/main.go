192b315eb5d6bbbb0a738d62ee885aa4cbee2c4a
Sachin Sankar <mail.sachinsankar@gmail.com>
2026-04-06 22:45:37 +0530

    feat: Basic prompt file parsing

--- cmd/dyp/main.go
+++ cmd/dyp/main.go
@@ -1,8 +1,58 @@
 // cmd/myapp/main.go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+)
+
+type Prompt struct {
+	title string
+	vars  []string
+}
+
+func listPromptFiles() []string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Unable to find user HOME directory")
+	}
+	promptsLocation := path.Join(home, ".prompts")
+	promptFiles, readError := os.ReadDir(promptsLocation)
+	if readError != nil {
+		log.Fatal("Unable to read .prompts directory")
+	}
+	var resultPromptFiles []string
+	for _, promptFile := range promptFiles {
+		resultPromptFiles = append(resultPromptFiles, path.Join(promptsLocation, promptFile.Name()))
+	}
+	return resultPromptFiles
+}
+
+func parsePromptFile(promptFilePath string) Prompt {
+	data, err := os.ReadFile(promptFilePath)
+	if err != nil {
+		log.Fatal("Unable to read file " + promptFilePath)
+	}
+	source := string(data)
+	parts := strings.Split(source, "---")
+	title := parts[0]
+	var vars []string
+	re := regexp.MustCompile(`\{\{(.+?)\}\}`)
+	matches := re.FindAllStringSubmatch(parts[1], -1)
+	for _, match := range matches {
+		vars = append(vars, match[1])
+	}
+	prompt := Prompt{title, vars}
+	return prompt
+}
 
 func main() {
-	fmt.Println("Hello!")
+	prompt := (parsePromptFile(listPromptFiles()[0]))
+	for _, i := range prompt.vars {
+		fmt.Println(i)
+	}
 }
